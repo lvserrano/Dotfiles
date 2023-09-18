f01f6cabd5967951592d99a565c934789638f330
Levi Serrano <75490600+lvserrano@users.noreply.github.com>
2023-09-18 13:59:17 -0300

    Arquivo completo

--- pacotes.sh
+++ pacotes.sh
@@ -12,6 +12,8 @@ echo ""
 git clone https://aur.archlinux.org/paru.git
 cd paru
 makepkg -si
+cd $HOME
+clear
 
 echo ""
 echo ""
@@ -22,6 +24,7 @@ echo ""
 echo ""
 
 sudo pacman -S --needed xorg xorg-server xorg-xbacklight xorg-fonts-misc xorg-xfd xorg-xkill xorg-xrandr xorg-xrdb xorg-xset xorg-xev xorg-xmodmap xorg-xwininfo xorg-xsetroot xorg-xinit libx11
+clear
 
 echo ""
 echo ""
@@ -30,9 +33,10 @@ echo "Instalando Drivers "
 echo ""
 echo ""
 echo ""
+sleep 3
 
 sudo pacman -S --needed xf86-video-fbdev xf86-video-vesa xf86-video-intel xorg-xinput xf86-input-libinput numlockx xf86-video-nouveau nvidia nvidia-utils nvidia-settings
-
+clear
   
 echo ""
 echo ""  
@@ -42,7 +46,9 @@ echo ""
 echo ""
 echo ""
 
+sleep 3
 sudo pacman -S --needed bspwm wmname sxhkd xclip i3-wm
+clear
 
 echo ""
 echo ""
@@ -52,7 +58,9 @@ echo ""
 echo ""
 echo ""
 
+sleep 3
 sudo pacman -S --needed inetutils networkmanager networkmanager-openvpn nethogs nm-connection-editor bluez bluez-utils blueman pulseaudio-bluetooth 
+clear
 
 echo ""
 echo ""
@@ -62,8 +70,9 @@ echo ""
 echo ""
 echo ""
 
+sleep 3
 sudo pacman -S --needed alsa-plugins alsa-tools alsa-utils pavucontrol pulseaudio pulseaudio-alsa pulseaudio-equalizer-ladspa viewnior gwenview ffmpeg ffmpegthumbnailer mpc mpd mplayer ncmpcpp tumbler feh
-
+clear
 
 echo ""
 echo ""
@@ -73,8 +82,9 @@ echo ""
 echo ""
 echo ""
 
+sleep 3
 sudo pacman -S --needed ranger highlight trash-cli ueberzug nemo nemo-preview nemo-seahorse nemo-fileroller zip p7zip unzip unrar xarchiver xdg-user-dirs xdg-user-dirs-gtk alacritty xfce4-terminal atril geany geany-plugins vim neovim kitty noto-fonts noto-fonts-emoji terminus-font ttf-dejavu
-
+clear
 
 echo ""
 echo ""
@@ -84,7 +94,9 @@ echo ""
 echo ""
 echo ""
 
+sleep 3
 sudo pacman -S --needed acpi autoconf automake binutils bison calc fakeroot gcc gparted gtk-engine-murrine gvfs gvfs-mtp gvfs-afc gvfs-gphoto2 gvfs-smb gvfs-google inotify-tools jq make patch pkg-config polkit polkit-kde-agent powertop python2 sshfs udisks2 wmctrl xclip xdotool xmlstarlet yad sndio
+clear
 
 echo ""
 echo ""
@@ -94,7 +106,9 @@ echo ""
 echo ""
 echo ""
 
+sleep 3
 sudo pacman -S --needed baobab dialog dunst gpick htop lxappearance meld ncdu nitrogen python-pywal rofi maim slop xsettingsd kvantum qt5ct open-vm-tools
+clear
 
 echo ""
 echo ""
@@ -104,8 +118,9 @@ echo ""
 echo ""
 echo ""
 
-sudo pacman -S --needed cava nemo-dropbox betterlockscreen downgrade i3lock-color ksuperkey light networkmanager-dmenu-git obmenu-generator perl-linux-desktopfiles polybar timeshift tlpui
-
+sleep 3
+paru -S cava nemo-dropbox betterlockscreen downgrade i3lock-color ksuperkey light networkmanager-dmenu-git obmenu-generator perl-linux-desktopfiles polybar timeshift tlpui
+clear
 
 echo "####################"
 echo "#     F E I T O    #"
